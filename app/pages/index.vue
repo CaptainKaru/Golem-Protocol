5df8b8be6d32ad6658c79de25b1d7b2dc3c00355
Carl Roy F. Arañez <carlroy.aranez02@gmail.com>
2026-03-10 20:29:08 +0800

    [update] Update content of index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,29 +1,37 @@
-<script setup lang="ts">
-    //import { useSeoMeta, useHead } from '@unhead/vue';
-    const title = "Golem Protocol | Home";
-    const description = "Golem Protocol is a responsive artificial intelligence system that adapts to your unique needs, learning from your behaviors and routines to provide seamless assistance across every aspect of your life.";
-    useSeoMeta({
-    title: () => title,
-    description: () => description,
-    charset: "utf-8",
-    viewport: "width=device-width, initial-scale=1.0"
-    });
-
-    useHead({
-    link: [
-    {rel: 'icon', type: 'image/png', href: '/logo.png'},
-    {rel: 'stylesheet', href: '/reset.css'}
-    ]
-    });
-</script>
-
+<script lang="ts"></script>
 <template>
     <client-only>
-        <NavBar />
-        <Welcome />
-        <Copyright />
+    <!-- <NavBar /> -->
+    <div id="PGMain" class="PG-main"></div>
+    <!-- <Cookies /> -->
+    <!-- <Copyright /> -->
     </client-only>
 </template>
 
+<script setup lang="ts">
+    import { useSeoMeta, useHead } from '@vueuse/head';
+    const title = "Project Golem| Home";
+    const description = "Golem Protocol is a responsive artificial intelligence system that adapts to your unique needs, learning from your behaviors and routines to provide seamless assistance across every aspect of your life.";
+    useSeoMeta({
+        title: () => title,
+        description: () => description,
+        charset: "utf-8",
+        viewport: "width=device-width, initial-scale=1.0"
+    });
+
+useHead({
+    link: [
+    {rel: 'icon', type: 'image/png', href: '/logo.png'},
+    {rel: 'stylesheet', href: '/reset.css'},
+    {rel: 'stylesheet', href: '/custom.css'} ,
+    {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+    {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
+    {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bitcount+Ink:wght@100..900&display=swap'}
+    ]
+});
+</script>
+
 <style scoped></style>
-<style></style>
+<style>
+/* Your scoped styles here */
+</style>
